518911ca1865178864fce58a1603e5d8fc58ff7d
Blanka <blanka@magestore.com>
2016-06-11 10:56:35 +0700

    magento 2 language vi_vn 1.0.0

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magestore_vi_vn',
+    __DIR__
+);
